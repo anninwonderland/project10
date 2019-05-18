814ad8625a1db048e411c78a954862cdd9779c2a
@anninwonderland <anninwonderland@yandex.ru>
2019-05-18 23:25:54 +0300

    UI started: UICollectionView and custom class for cell were added

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ann on 18/05/2019.
+//  Copyright © 2019 Ann. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell { //создаем класс для отдельного элемента интерфейса – ячейки
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
